2e586d4b6ec0e11010e042fe699033a170cb20b6
marcosBusto <117916043+marcosBusto@users.noreply.github.com>
2023-01-15 18:14:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const engine = require('ejs-mate');
 const path = require('path');
 const mongoose = require('mongoose');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
-const morgan = require('morgan');
 
 //initializations
 const app = express();
@@ -16,18 +12,6 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 //app.set('views', '')
-
-//middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
-app.use(session({
-  secret: 'mysecretsession',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
 
 // routes
 app.use(require('./routes/index'));
