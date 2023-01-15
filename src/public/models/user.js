7d3eba20abe5b0d0a849f660459851b7955ef570
marcosBusto <117916043+marcosBusto@users.noreply.github.com>
2023-01-15 16:32:04 +0100

    summary

--- src/public/models/user.js
+++ src/public/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 Schema = mongoose.Schema;
 
@@ -6,5 +7,13 @@ var userSchema = new Schema({
     email: String,
     password: String
 });
+
+userSchema.methods.encryptPassword = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  };
+  
+  userSchema.methods.comparePassword= function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
 
 module.exports = mongoose.model('users', userSchema);
